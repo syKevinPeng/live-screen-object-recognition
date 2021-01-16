db454422660a3b233bbd83977ea05290200be2e4
Siyuan Peng <siyuan.kevin.peng@gmail.com>
2021-01-16 16:07:33 -0500

    able to detect object but not accurate

--- on_screen_overlay.py
+++ on_screen_overlay.py
@@ -1,40 +1,47 @@
 import tkinter as tk
 from tkinter import Canvas
 import numpy as np
-import cv2, time
+import cv2, time, util
 import tensorflow as tf
 from tensorflow.python import saved_model
 import detector
 
 class FullScreen(object):
-    def __init__(self, master, queue, **kwargs):
-        self.master=master
+    def __init__(self, window, queue, **kwargs):
+        self.window = window
         self.queue = queue
-        master.geometry("{0}x{1}+0+0".format(
-            master.winfo_screenwidth(), master.winfo_screenheight()))
+        self.window.geometry("{0}x{1}+0+0".format(
+            self.window.winfo_screenwidth(), self.window.winfo_screenheight()))
+        self.window.wait_visibility(window)
+        self.window.wm_attributes("-alpha", 0.4)
+        self.canvas = Canvas(self.window, width= self.window.winfo_screenwidth(), height = self.window.winfo_screenheight())
+        # self.window.config(bg='#000000')
+        # self.window.wm_attributes("-topmost", 1)
+        # self.window.attributes('-transparentcolor', '#000000', '-topmost', 1)
+
+        # Configuring bg
+        # self.bg = Canvas(self.window, highlightthickness=0)
+        # self.setClickthrough(self.bg.winfo_id())
 
     def draw_box(self, bbox, image):
         out_boxes, out_scores, out_classes, num_boxes = bbox
         image_h, image_w, _ = image.shape
-        canvas = Canvas(self.master)
-        canvas.pack()
+        classes = util.read_class_names("./tensorflow-yolov4-tflite/data/classes/coco.names")
+        num_classes = len(classes)
         for i in range(num_boxes[0]):
-            if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > self.num_classes: continue
+            if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > num_classes: continue
             coord = out_boxes[0][i]
             coord[0] = int(coord[0] * image_h)
             coord[2] = int(coord[2] * image_h)
             coord[1] = int(coord[1] * image_w)
             coord[3] = int(coord[3] * image_w)
-            canvas.create_rectangle([coord[0],coord[1],coord[2],coord[3]], width=2, outline = 'black')
+            print("bbox: \n", [coord[1], coord[0], coord[3], coord[2]])
+            self.canvas.create_rectangle(coord[1], coord[0], coord[3], coord[2], width=2, outline='black')
+            # canvas.create_rectangle(50, 0, 100, 50, width=2, outline='black')
+        self.canvas.pack()
 
-    def queue_monitor(self):
-        while(True):
-            if(not self.queue.empty()):
-                self.queue.put([])
-                pred_bbox, image = self.queue.get()
-                print(image)
-                self.queue.get()
-            time.sleep(0.2)
+    def clean_canvas(self):
+        self.canvas.delete("all")
 
 
 
--- run.py
+++ run.py
@@ -1,18 +1,27 @@
 import on_screen_overlay
 import util
 import tkinter as tk
+from tkinter import Canvas
 from multiprocessing import Process, Queue
 import colorsys, random
 import time
 
 def application(queue):
     window = tk.Tk()
-    window.wait_visibility(window)
-    window.wm_attributes("-alpha", 0.4)
     app = on_screen_overlay.FullScreen(window, queue)
-    app.queue_monitor()
+    # app.queue_monitor()
     window.bind("<Escape>", lambda x: window.destroy())
-    window.mainloop()
+    # window.mainloop()
+    while(True):
+        if (not queue.empty()):
+            queue.put([])# place holder
+            pred_bbox, image = queue.get()
+            app.clean_canvas()
+            app.draw_box(pred_bbox,image)
+            queue.get()
+        time.sleep(0.2)
+        window.update_idletasks()
+        window.update()
 
 
 def capture_and_detect(queue):
@@ -20,12 +29,12 @@ def capture_and_detect(queue):
         if(queue.empty()):
             pred_bbox, image = util.capture_screen()
             queue.put([pred_bbox, image])
-            print(queue)
         time.sleep(0.2)
 
 
 
 if __name__ == "__main__":
+    # create multiprocessing queue for passing bbox between child process
     bbox_queue = Queue()
     app_process = Process(target= application, args=(bbox_queue,))
     detect_process = Process(target= capture_and_detect, args=(bbox_queue,))
