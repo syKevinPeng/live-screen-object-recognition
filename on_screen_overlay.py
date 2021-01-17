1371137a09916f9a0ea826659b00847852c55155
Siyuan Peng <siyuan.kevin.peng@gmail.com>
2021-01-17 14:07:21 -0500

    improve detection efficienty in multiprocessing

--- on_screen_overlay.py
+++ on_screen_overlay.py
@@ -1,27 +1,18 @@
 import tkinter as tk
 from tkinter import Canvas
 import numpy as np
-import cv2, time, util
-import tensorflow as tf
-from tensorflow.python import saved_model
-import detector
+import util
 
 class FullScreen(object):
-    def __init__(self, window, queue, **kwargs):
+    def __init__(self, window, **kwargs):
         self.window = window
-        self.queue = queue
         self.window.geometry("{0}x{1}+0+0".format(
             self.window.winfo_screenwidth(), self.window.winfo_screenheight()))
         self.window.wait_visibility(window)
         self.window.wm_attributes("-alpha", 0.4)
         self.canvas = Canvas(self.window, width= self.window.winfo_screenwidth(), height = self.window.winfo_screenheight())
         # self.window.config(bg='#000000')
-        # self.window.wm_attributes("-topmost", 1)
-        # self.window.attributes('-transparentcolor', '#000000', '-topmost', 1)
-
-        # Configuring bg
-        # self.bg = Canvas(self.window, highlightthickness=0)
-        # self.setClickthrough(self.bg.winfo_id())
+        self.window.wm_attributes("-topmost", 1)
 
     def draw_box(self, bbox, image):
         out_boxes, out_scores, out_classes, num_boxes = bbox
@@ -37,7 +28,6 @@ class FullScreen(object):
             coord[3] = int(coord[3] * image_w)
             print("bbox: \n", [coord[1], coord[0], coord[3], coord[2]])
             self.canvas.create_rectangle(coord[1], coord[0], coord[3], coord[2], width=2, outline='black')
-            # canvas.create_rectangle(50, 0, 100, 50, width=2, outline='black')
         self.canvas.pack()
 
     def clean_canvas(self):
--- run.py
+++ run.py
@@ -3,45 +3,73 @@ import util
 import tkinter as tk
 from tkinter import Canvas
 from multiprocessing import Process, Queue
-import colorsys, random
+import detector
 import time
 
-def application(queue):
+# def application(queue):
+#     window = tk.Tk()
+#     app = on_screen_overlay.FullScreen(window, queue)
+#     # app.queue_monitor()
+#     window.bind("<Escape>", lambda x: window.destroy())
+#     # window.mainloop()
+#     while ...:
+#         if queue:
+#             queue.put([])# place holder
+#             pred_bbox, image = queue.get()
+#             app.clean_canvas()
+#             app.draw_box(pred_bbox,image)
+#             queue.get()
+#         time.sleep(0.2)
+#         window.update_idletasks()
+#         window.update()
+#
+#
+# def capture_and_detect(queue):
+#     while ...:
+#         if(queue.empty()):
+#             pred_bbox, image = util.capture_screen()
+#             queue.put([pred_bbox, image])
+#         time.sleep(0.2)
+def application(bbox_queue,img_queue):
     window = tk.Tk()
-    app = on_screen_overlay.FullScreen(window, queue)
+    app = on_screen_overlay.FullScreen(window)
     # app.queue_monitor()
     window.bind("<Escape>", lambda x: window.destroy())
-    # window.mainloop()
-    while(True):
-        if (not queue.empty()):
-            queue.put([])# place holder
-            pred_bbox, image = queue.get()
-            app.clean_canvas()
-            app.draw_box(pred_bbox,image)
-            queue.get()
-        time.sleep(0.2)
+    while ...:
+        image = util.capture_screen()
+        img_queue.put(image)
+
+        # when detect process is detecting, wait
+        while bbox_queue.empty():
+            time.sleep(0.1)
+        bbox = bbox_queue.get()
+        app.clean_canvas() # clean the bbox from previous frame
+        app.draw_box(bbox, image)
         window.update_idletasks()
         window.update()
 
-
-def capture_and_detect(queue):
-    while (True):
-        if(queue.empty()):
-            pred_bbox, image = util.capture_screen()
-            queue.put([pred_bbox, image])
-        time.sleep(0.2)
+def detect(bbox_queue,img_queue):
+    # initialize yolo
+    yolo = detector.YoloDetector()
+    while ...:
+        while img_queue.empty():
+            pass
+        img = img_queue.get()
+        bbox = yolo.detect(img)
+        bbox_queue.put(bbox)
 
 
 
 if __name__ == "__main__":
-    # create multiprocessing queue for passing bbox between child process
+    # create multiprocessing queue for passing bbox and captured screen image between child process
     bbox_queue = Queue()
-    app_process = Process(target= application, args=(bbox_queue,))
-    detect_process = Process(target= capture_and_detect, args=(bbox_queue,))
+    img_queue = Queue()
+    app_process = Process(target= application, args=(bbox_queue,img_queue,))
+    detect_process = Process(target= detect, args=(bbox_queue,img_queue,))
 
     app_process.start()
     detect_process.start()
 
     app_process.join()
-    detect_process.terminate()
+    detect_process.terminate() # once the app process stops, terminate detect process immediately
     detect_process.join()
